809c18b02a0ec587e0db38ad5203f09879a80a44
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-04-30 19:36:54 +0000

    did a sequence with imput from user

---
+++ sequence.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Created by: Lauren Wheatley
+# Created on: April 2021
+# This program calculates total from subtotal and tax
+
+
+def main():
+    # this function adds 2 numbers together with user input
+
+    # input
+    f = int(input("Enter the first number: "))
+    s = int(input("Enter the second number: "))
+
+    # process
+    total = f + s
+
+    # output
+    print("")
+    print("The answer is {}".format(f + s))
+
+
+if __name__ == "__main__":
+    main()
